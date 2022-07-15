a6880a72c1f558664652964aeff7a216692eea6e
johnpathadan <johnpathadan@gmail.com>
2022-07-15 12:39:58 +0530

    click events added

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
+import React from "react";
+
 const Home = () => {
+    const handleClick = (e) =>{
+        console.log(e);
+    }
+    const handleClickAgain = (name, e)=>{
+        console.log(`Hi, my name is ${name}`, e.target);
+    }
+
     return (
         <div className="homepage">
-            <h2>Homepage</h2>
+            <h2 className="title">Homepage</h2>
+            <button onClick={handleClick}>Clicke Me</button>
+            <button onClick={(e)=> handleClickAgain('Yoshi', e)}>Click Me Again</button>
         </div>
     );
 }
