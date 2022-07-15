b373c6ed5bd886fa1ba22186d4556abbc4404dd6
johnpathadan <johnpathadan@gmail.com>
2022-07-15 11:50:00 +0530

    basic styling added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import logo from './logo.svg';
-import './App.css';
 import NavBar from './NavBar';
 import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  const links = 'http://google.com';
   return (
     <div className='App'>
      <NavBar />
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,7 +1,7 @@
 const NavBar = () => {
     return (
         <nav className="navbar">
-            <h2>The Dojo Blog</h2>
+            <h2>News Blog</h2>
             <div className="links">
                 <a href="/">Home</a>
                 <a href="/create">New Blog</a>
