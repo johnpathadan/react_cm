876c7e15ebe3e77dc0a9dd89113163e864cf5d71
johnpathadan <johnpathadan@gmail.com>
2022-07-15 11:37:56 +0530

    about to add styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './NavBar';
+import Home from './Home';
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  const links = 'http://google.com';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+     <NavBar />
+     <div className="content">
+       <Home />
+     </div>
     </div>
   );
 }
---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return (
+        <nav className="navbar">
+            <h2>The Dojo Blog</h2>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default NavBar;
