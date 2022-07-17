c015dee6403e645f0862bca7cafcf0d8b9482f2b
johnpathadan <johnpathadan@gmail.com>
2022-07-17 19:27:47 +0530

    sunday commit

--- src/Home.js
+++ src/Home.js
@@ -3,20 +3,35 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const[name, setName] = useState('mario');
+    
     const[blogs, setBlogs] = useState(null);
+    const[isPending, setIsPending] = useState(true);
+    const[error, setError] = useState(null);
     
     useEffect(()=>{
-        fetch('http://localhost:8000/blogs')
+        setTimeout(()=>{
+            fetch('http://localhost:8000/blogs')
             .then((res)=>{
+                if(!res.ok){
+                    throw('could not fetch the data for that resource');
+                }
                 return res.json();
             }).then((data)=>{
-                return setBlogs(data);
+                setBlogs(data);
+                setIsPending(false);
+                setError(null);
+            }).catch(err=>{
+                setIsPending(false);
+                // console.log(err.message);
+                setError(err.message);
             })
-    },[name])
+        }, 1000)
+    },[])
 
     return (
         <div className="homePage">
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
            {blogs && <BlogList blogs={blogs} title='All Blogs!!'/>}
         </div>
     );
@@ -24,3 +39,4 @@ const Home = () => {
  
 
 export default Home;
+
