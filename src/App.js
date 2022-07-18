e24c86c08de356700990682a689b0b07a553cd3a
johnpathadan <johnpathadan@gmail.com>
2022-07-18 15:15:24 +0530

    react router command added in app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-
 import NavBar from './NavBar';
 import Home from './Home';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
-    <div className='App'>
+    <Router>
+      <div className='App'>
      <NavBar />
      <div className="content">
-       <Home />
+       <Switch>
+        <Route path='/'>
+        <Home />
+        </Route>
+       </Switch>
      </div>
     </div>
+    </Router>
   );
 }
 
